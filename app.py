3b8393cc01408205b8f8440cf6df98e02abc121a
Anavsan2 <160516768+Anavsan2@users.noreply.github.com>
2026-02-19 12:28:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ import requests
 import msal
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
 
 # --- 1. SISTEMA DE CONTRASEÑA ---
